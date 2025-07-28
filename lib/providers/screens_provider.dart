b56797b8ea5f33e3e9b774efce4fda37e7137aba
Muflih-uk <muhammedmuflih875@gmail.com>
2025-07-28 20:19:16 +0530

    Root Layout AppBar and BottomNavigationBar added

---
+++ lib/providers/screens_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScreensProvider extends ChangeNotifier{
+  int _currentIndex = 0;
+  int get currentIndex => _currentIndex;
+
+  void changePage(int index){
+    _currentIndex = index;
+    notifyListeners();
+  }
+}
---
+++ lib/views/screens/root_layout.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:pgbee/core/constants/colors.dart';
+import 'package:pgbee/providers/screens_provider.dart';
+import 'package:provider/provider.dart';
+
+class RootLayout extends StatefulWidget{
+  @override
+  State<RootLayout> createState() => _StateRootLayout(); 
+}
+
+class _StateRootLayout extends State<RootLayout> {
+
+  final List<Widget> _screens = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("PG Details"),
+    ),
+    Center(
+      child: Text("INbox"),
+    ),
+    Center(
+      child: Text("Profile"),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final pageProvider = Provider.of<ScreensProvider>(context);
+    int selectedIndex = pageProvider.currentIndex;
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/images/logo.png',
+          width: 230,
+          height: 40,
+          fit: BoxFit.fill,
+        ),
+        centerTitle: true,
+      ),
+      body: _screens[selectedIndex],
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: BottomNavigationBar(
+            onTap: pageProvider.changePage,
+            selectedIconTheme: IconThemeData(
+              color: LightColor.background,
+            ),
+            unselectedIconTheme: IconThemeData(
+              color: LightColor.grey
+            ),
+            type: BottomNavigationBarType.fixed,
+            currentIndex: selectedIndex,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            backgroundColor: LightColor.black,
+            selectedItemColor: LightColor.background,
+            unselectedItemColor: LightColor.grey,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                  size: 30,
+                ),
+                label: "Home",           
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.description_outlined,
+                  size: 30,
+                ),
+                label: "PG Details"
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.mail_outline,
+                  size: 30,
+                ),
+                label: "Inbox"
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_2_outlined,
+                  size: 30,
+                ),
+                label: "Profile"
+              ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
